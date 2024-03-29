55575cc73397d4aed17222e3fe68521f5ab20508
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-03-29 14:17:23 +0200

    fixing unittests

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 Defines a route to show the status of the web app
 Defines a route to show the states of the db
 """
+from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
@@ -22,10 +23,10 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """ that retrieves the number of each objects by type """
-    return {'amenities': storage.count(Amenity),
+    return jsonify({'amenities': storage.count(Amenity),
             'cities': storage.count(City),
             'places': storage.count(Place),
             'reviews': storage.count(Review),
             'states': storage.count(State),
             'users': storage.count(User),
-            }
+            })
