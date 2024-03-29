a163bc92a993c52caacbd16dc632fbdea3eb115e
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-03-29 14:28:43 +0200

    style edit for index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -23,17 +23,10 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """ that retrieves the number of each objects by type """
-    classes = {
-        "amenities": Amenity,
-        "cities": City,
-        "places": Place,
-        "reviews": Review,
-        "states": State,
-        "users": User
-        }
-    stats_dict = {
-        key: storage.count(value)
-        for key, value in classes.items()
-        if storage.count(value)
-        }
-    return jsonify(stats_dict)
+    return jsonify({'amenities': storage.count(Amenity),
+                    'cities': storage.count(City),
+                    'places': storage.count(Place),
+                    'reviews': storage.count(Review),
+                    'states': storage.count(State),
+                    'users': storage.count(User),
+                    })
