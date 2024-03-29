40a808c89dab26d02b6c16eee4ccaa2ad68efc24
Mohamed Eladly <msrtv365@yahoo.com>
2024-03-29 08:44:38 +0200

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, make_response
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -7,6 +7,11 @@ from os import environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(err):
+    return make_response({'error': 'Not found'}, 404)
 
 
 @app.teardown_appcontext
